3fb2abbf7296840dfdf84bf6c9c2aabc707f5fcd
Ernest Lebedev <allesverloren133@gmail.com>
2019-06-15 18:22:03 +0300

    Added Vue Bootstrap and Vue Router

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
+
+import router from './router';
 import App from './components/App';
 
 Vue.prototype.$config = APP_CONFIG;
 
 new Vue({
     el: '#app',
+    router: router,
     components: {
         App
     }
