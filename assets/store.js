b8bb9585f1e13d61acb0c51a9ee41acc6ff9d123
Ernest Lebedev <allesverloren133@gmail.com>
2019-06-17 21:07:56 +0300

    Implemented Vue store

---
+++ assets/store.js
@@ -0,0 +1,81 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import api from './api';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        title: '',
+        isLoading: false,
+        error: null,
+        login: null
+    },
+    getters: {
+        title (state) {
+            return state.title;
+        },
+        isLoading (state) {
+            return state.isLoading;
+        },
+        hasError (state) {
+            return (state.error !== null);
+        },
+        error (state) {
+            return state.error;
+        },
+        isAuthenticated (state) {
+            return !!state.login;
+        },
+        login (state) {
+            return state.login;
+        }
+    },
+    mutations: {
+        ['INITIALIZING_DATA'](state, data) {
+            state.title = data.title;
+            state.login = data.login;
+        },
+        ['AUTHENTICATING'](state) {
+            state.isLoading = true;
+            state.error = null;
+            state.login = null;
+        },
+        ['AUTHENTICATING_SUCCESS'](state, data) {
+            state.isLoading = false;
+            state.error = null;
+            state.login = data.login;
+        },
+        ['AUTHENTICATING_ERROR'](state, error) {
+            state.isLoading = false;
+            state.error = error.data.message;
+            state.login = null;
+        },
+        ['LOGGING_OUT'](state) {
+            state.isLoading = true;
+        },
+        ['LOGGING_OUT_SUCCESS'](state) {
+            state.isLoading = false;
+            state.login = null;
+        }
+    },
+    actions: {
+        init ({ commit }, payload) {
+            commit('INITIALIZING_DATA', payload);
+        },
+        login ({ commit }, payload) {
+            commit('AUTHENTICATING');
+
+            return api.login(payload.login, payload.password)
+                .then(result => commit('AUTHENTICATING_SUCCESS', result.data))
+                .catch(error => commit('AUTHENTICATING_ERROR', error.response));
+        },
+        logout ({ commit }) {
+            commit('LOGGING_OUT');
+
+            return api.logout()
+                .then(() => commit('LOGGING_OUT_SUCCESS'));
+        }
+    }
+});
