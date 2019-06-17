779d8724ceda1698ddf01141d5d0ed725f73672b
Ernest Lebedev <allesverloren133@gmail.com>
2019-06-17 21:00:03 +0300

    Implemented Vue router

---
+++ assets/router.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import store from './store';
+import Home from './components/Home';
+import Login from './components/Login';
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: 'history',
+    routes: [
+        { path: '/', component: Home, meta: { requiresAuth: true } },
+        { path: '/login', component: Login },
+        { path: '*', redirect: '/' }
+    ],
+    scrollBehavior: function (to, from, savedPosition) {
+        return (savedPosition || { x: 0, y: 0 });
+    }
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.getters.isAuthenticated) {
+            next('/login');
+        }
+        else {
+            next();
+        }
+    }
+    else {
+        next();
+    }
+});
+
+export default router;
