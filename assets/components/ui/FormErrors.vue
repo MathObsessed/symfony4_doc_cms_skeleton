f669a2fef5838a388bc0d9c15bf77fd0e84b553b
Ernest Lebedev <allesverloren133@gmail.com>
2019-06-19 13:06:07 +0300

    Added FormErrors component

---
+++ assets/components/ui/FormErrors.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="alert alert-danger" role="alert">
+        {{ errorMessage }}
+        <ul v-if="errors.length > 0">
+            <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FormErrors',
+        props: {
+            errorMessage: {
+                type: String,
+                required: true
+            },
+            errors: {
+                type: Array,
+                required: true
+            }
+        }
+    };
+</script>
